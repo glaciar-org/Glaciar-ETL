842aaa9356bdea65d02324051e9d76f01cc611ab
Pablo Ezequiel Inchausti <pinchausti@uade.edu.ar>
2019-07-28 15:33:48 -0300

    ID-#2 ETL Automatico

--- exchange_data/dispatch.sh
+++ exchange_data/dispatch.sh
@@ -2,6 +2,18 @@
 
 echo "Runnig: dispatch $1 "
 
+function do_dispatch 
+{
+    echo " > [do_dispatch $1] (Expected ETL1-DS01 .. ETL5-DS05 )"
+
+    cd ../ETL-pipelines/$1; 
+                     ./run-00-clean.sh;
+                     ./run-01-download.sh; 
+                     ./run-02-transform.sh; 
+                     ./run-03-mongo.sh; 
+     cd ../../exchange_data
+}
+
 if [ -z "$1" ] || [ $1 == "HELP" ]
 then
     echo "Usage: dispatch [ ETL1 | ETL2 | ETL3 | ETL4 | ETL5 | RESTORE | HELP (default) ]"
@@ -10,26 +22,31 @@ fi
 
 if [ $1 == "ETL1" ]; then
     echo "Work for ETL1"
+    do_dispatch "ETL1-DS01"
     exit 0
 fi
 
 if [ $1 == "ETL2" ]; then
     echo "Work for ETL2"
+    do_dispatch "ETL1-DS02"
     exit 0
 fi
 
 if [ $1 == "ETL3" ]; then
     echo "Work for ETL3"
+    do_dispatch "ETL1-DS03"
     exit 0
 fi
 
 if [ $1 == "ETL4" ]; then
     echo "Work for ETL4"
+    do_dispatch "ETL1-DS04"
     exit 0
 fi
 
 if [ $1 == "ETL5" ]; then
     echo "Work for ETL5"
+    do_dispatch "ETL1-DS05"
     exit 0
 fi
 
--- run-all.sh
+++ run-all.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
-
-  glaciar-org-mongo-etl:
-    container_name: glaciar-org-mongo-etl
-    image: glaciar/glaciar.org-etl:1.0
-    env_file:
-      - ./.env_docker_swarm
-    configs:
-      - source:  glaciar_config
-        target: /glaciar_config.json
-    networks:
-      - network-back
-
-
-cd ./jenkins-pipelines-prod/ETL1-DS01; ./run-00-clean.sh; ./run-01-download.sh; ./run-02-transform.sh; ./run-03-mongo.sh; cd ../..
-cd ./jenkins-pipelines-prod/ETL2-DS02; ./run-00-clean.sh; ./run-01-download.sh; ./run-02-transform.sh; ./run-03-mongo.sh; cd ../..
-cd ./jenkins-pipelines-prod/ETL3-DS03; ./run-00-clean.sh; ./run-01-download.sh; ./run-02-transform.sh; ./run-03-mongo.sh; cd ../..
-cd ./jenkins-pipelines-prod/ETL4-DS04; ./run-00-clean.sh; ./run-01-download.sh; ./run-02-transform.sh; ./run-03-mongo.sh; cd ../..
-cd ./jenkins-pipelines-prod/ETL5-DS05; ./run-00-clean.sh; ./run-01-download.sh; ./run-02-transform.sh; ./run-03-mongo.sh; cd ../..
+cd ./ETL-pipelines/ETL1-DS01; ./run-00-clean.sh; ./run-01-download.sh; ./run-02-transform.sh; ./run-03-mongo.sh; cd ../..
+cd ./ETL-pipelines/ETL2-DS02; ./run-00-clean.sh; ./run-01-download.sh; ./run-02-transform.sh; ./run-03-mongo.sh; cd ../..
+cd ./ETL-pipelines/ETL3-DS03; ./run-00-clean.sh; ./run-01-download.sh; ./run-02-transform.sh; ./run-03-mongo.sh; cd ../..
+cd ./ETL-pipelines/ETL4-DS04; ./run-00-clean.sh; ./run-01-download.sh; ./run-02-transform.sh; ./run-03-mongo.sh; cd ../..
+cd ./ETL-pipelines/ETL5-DS05; ./run-00-clean.sh; ./run-01-download.sh; ./run-02-transform.sh; ./run-03-mongo.sh; cd ../..
